c0521064589e685f3e18478a23f4dd49c98ef374
Blaine <blainemalone01@gmail.com>
2024-06-24 20:56:18 -0400

    fix: buy crypto button along with clean up

---
+++ web/src/useScript.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+const useScript = (src: string) => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [src]);
+};
+
+export default useScript;
