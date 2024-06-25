33d214cfac3256a7ca2b39984a1bf6b149743ced
Blaine <blainemalone01@gmail.com>
2024-06-24 20:28:38 -0400

    fix: routing

---
+++ web/src/BuyCrypto.tsx
@@ -0,0 +1,15 @@
+// src/BuyCrypto.tsx
+import React from 'react';
+import { Box, Center, Text } from "@chakra-ui/react";
+
+const BuyCrypto: React.FC = () => {
+  return (
+    <Center>
+      <Box p={4} borderWidth="1px" borderRadius="lg" textAlign="center">
+        <Text fontSize="2xl">This is a simple page!</Text>
+      </Box>
+    </Center>
+  );
+};
+
+export default BuyCrypto;
