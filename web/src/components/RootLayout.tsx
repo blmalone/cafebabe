db88e85bc07d300afb3835f67cb1c75c816a7491
Olivia Barnett <oliviabarnett@me.com>
2024-06-25 22:35:12 -0400

    format

--- web/src/components/RootLayout.tsx
+++ web/src/components/RootLayout.tsx
@@ -1,34 +1,35 @@
 // src/components/RootLayout.tsx
-import { ReactNode } from 'react';
-import { OnchainKitProvider } from '@coinbase/onchainkit';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { base, baseSepolia } from 'wagmi/chains';
-import { WagmiProvider, createConfig, http } from 'wagmi';
-import { coinbaseWallet } from 'wagmi/connectors';
+import { ReactNode } from "react";
+import { OnchainKitProvider } from "@coinbase/onchainkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { base, baseSepolia } from "wagmi/chains";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { coinbaseWallet } from "wagmi/connectors";
 
 type Props = { children: ReactNode };
 
-const rpcUrl = "https://api.developer.coinbase.com/rpc/v1/base/0dD2uvTjYG7Wp6QfzenzTi_OiY_Of91P";
-const baseUrl = rpcUrl.replace(/\/v1\/(.+?)\//, '/v1/base/');
-const baseSepoliaUrl = rpcUrl.replace(/\/v1\/(.+?)\//, '/v1/base-sepolia/');
+const rpcUrl =
+  "https://api.developer.coinbase.com/rpc/v1/base/0dD2uvTjYG7Wp6QfzenzTi_OiY_Of91P";
+const baseUrl = rpcUrl.replace(/\/v1\/(.+?)\//, "/v1/base/");
+const baseSepoliaUrl = rpcUrl.replace(/\/v1\/(.+?)\//, "/v1/base-sepolia/");
 
 const queryClient = new QueryClient();
 
 const wagmiConfig = createConfig({
-    chains: [base, baseSepolia],
-    connectors: [
-        coinbaseWallet({
-            appName: 'cafebabe',
-            preference: 'all', // Setting this as all gives you the QR code for scanning with Coinbase wallet.
-            chainId: base.id,
-        }),
-    ],
-    ssr: false,
-    transports: {
-        [base.id]: http(baseUrl),
-        [baseSepolia.id]: http(baseSepoliaUrl),
-    },
-})
+  chains: [base, baseSepolia],
+  connectors: [
+    coinbaseWallet({
+      appName: "cafebabe",
+      preference: "all", // Setting this as all gives you the QR code for scanning with Coinbase wallet.
+      chainId: base.id,
+    }),
+  ],
+  ssr: false,
+  transports: {
+    [base.id]: http(baseUrl),
+    [baseSepolia.id]: http(baseSepoliaUrl),
+  },
+});
 
 function RootLayout({ children }: Props) {
   return (
