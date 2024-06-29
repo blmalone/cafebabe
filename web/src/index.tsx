0c2a6ac4c89b0aa594704754bc3f1f6338eea0ee
Blaine <blainemalone01@gmail.com>
2024-06-29 16:54:37 -0400

    fix: hash routing

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,16 +1,55 @@
-// src/index.tsx
+// // src/index.tsx
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { HashRouter } from 'react-router-dom';
+// import App from './pages/App';
+import RootLayout from './components/RootLayout';
+
+// const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+// root.render(
+//   <RootLayout>
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   </RootLayout>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './pages/App';
-import RootLayout from './components/RootLayout';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <RootLayout>
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//     </RootLayout>
+//   </React.StrictMode>
+// );
 
-root.render(
-  <RootLayout>
+
+
+import {
+  createHashRouter
+} from 'react-router-dom';
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* <RouterProvider router={router} /> */}
+    <RootLayout>
     <HashRouter>
       <App />
     </HashRouter>
-  </RootLayout>
+    </RootLayout>
+  </React.StrictMode>
 );
